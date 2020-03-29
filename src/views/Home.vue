5b42d392f65b83f92bc50393c4262498369110ca
luthfywiratama <48203030+luthfywiratama@users.noreply.github.com>
2020-03-30 04:02:55 +0700

    update

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroShayna />
+    <WomenBanner />
+    <InstagramShayna />
+    <PartnerShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import HeroShayna from '@/components/HeroShayna.vue'
+import WomenBanner from '@/components/WomenBanner.vue'
+import InstagramShayna from '@/components/InstagramShayna.vue'
+import PartnerShayna from '@/components/PartnerShayna.vue'
+import FooterShayna from '@/components/FooterShayna.vue'
+
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    WomenBanner,
+    InstagramShayna,
+    PartnerShayna,
+    FooterShayna
+  }
+}
+</script>
